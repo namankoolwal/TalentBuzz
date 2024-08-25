68a55e35cfa2dc3a3d11292e397cb05a5d7bed97
Naman Khandelwal <namankoolwal1411@gmail.com>
2024-08-25 19:47:00 +0530

    complete about us

--- src/components/AboutUs/ImageSection.jsx
+++ src/components/AboutUs/ImageSection.jsx
@@ -1,28 +1,28 @@
 
 
 // *************************Way one**********************************
-/* eslint-disable no-unused-vars */
-import React from 'react'
-import card2 from '../../assets/images/AboutUs/CardSection/card2.png'
+// /* eslint-disable no-unused-vars */
+// import React from 'react'
+// import card2 from '../../assets/images/AboutUs/CardSection/card2.png'
 
 
-const ImageSection = () => {
-  return (
-    <div className='px-[5.5vw] my-[60px] md:my-[120px] h-full'>
-    <div className='flex h-[40vmin] md:h-[70vmin] w-full '>
+// const ImageSection = () => {
+//   return (
+//     <div className='px-[5.5vw] my-[60px] md:my-[120px] h-full'>
+//     <div className='flex h-[40vmin] md:h-[70vmin] w-full '>
 
-    {Array.from({ length: 6 }).map((_, i) => (
-      <div key={i} className=' w-full rounded-full h-full text-yellow-400 overflow-hidden'>
-        <img src={card2} alt="" className='w-full h-full object-cover object-center' />
-      </div>
-    ))}
+//     {Array.from({ length: 6 }).map((_, i) => (
+//       <div key={i} className=' w-full rounded-full h-full text-yellow-400 overflow-hidden'>
+//         <img src={card2} alt="" className='w-full h-full object-cover object-center' />
+//       </div>
+//     ))}
         
-    </div>
-    </div>
-  )
-}
+//     </div>
+//     </div>
+//   )
+// }
 
-export default ImageSection
+// export default ImageSection
 
 
 
@@ -64,23 +64,23 @@ export default ImageSection
 
 
 // ***************************************Way 3*************************************
-// /* eslint-disable no-unused-vars */
-// import React from 'react'
-// import card2 from '../../assets/images/AboutUs/CardSection/card2.png'
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import card2 from '../../assets/images/AboutUs/CardSection/card2.png'
 
-// const ImageSection = () => {
-//     return (
-//         <div className='px-[5.5vw] my-[60px] md:my-[120px] h-full '>
-//             <div
-//                 className='relative h-[40vmin] md:h-[70vmin] bg-cover bg-center bg-no-repeat image-section'
-//                 style={{ backgroundImage: `url(${card2})` }}
-//             >
+const ImageSection = () => {
+    return (
+        <div className='px-[5.5vw] my-[60px] md:my-[120px] h-full '>
+            <div
+                className='relative h-[40vmin] md:h-[70vmin] bg-cover bg-center bg-no-repeat image-section'
+                style={{ backgroundImage: `url(${card2})` }}
+            >
 
 
-//             </div>
+            </div>
 
-//         </div>
-//     )
-// }
+        </div>
+    )
+}
 
-// export default ImageSection
+export default ImageSection
