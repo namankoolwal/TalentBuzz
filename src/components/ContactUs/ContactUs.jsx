b6fa77066b14e800e12d1038a8d4a07a27e0fd9f
Naman Khandelwal <namankoolwal1411@gmail.com>
2024-08-29 14:51:35 +0530

    add contact us page

---
+++ src/components/ContactUs/ContactUs.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const ContactUs = () => {
+  return (
+    <div className='w-full min-h-screen h-full'>
+      <div className='text-3xl text-center mt-20'>Hello From contact Us page..</div>
+    </div>
+  )
+}
+
+export default ContactUs
--- src/router.jsx
+++ src/router.jsx
@@ -4,6 +4,7 @@ import Layout from "./Layout";
 import LandingPage from "./components/LandingPage/LandingPage";
 import AboutUs from "./components/AboutUs/AboutUs";
 import Features from "./components/Features/Features";
+import ContactUs from "./components/ContactUs/ContactUs";
 
 
 const router = createBrowserRouter(
@@ -12,7 +13,7 @@ const router = createBrowserRouter(
             <Route path="" element={<LandingPage/>} />
             <Route path="features" element={<Features/>} />
             <Route path="about" element={<AboutUs/>} />
-            <Route path="contact" element={<> <div>Hello contact</div></>} />
+            <Route path="contact" element={<ContactUs/>} />
 
         </Route>
     )
