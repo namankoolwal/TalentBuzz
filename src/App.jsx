2c9a81e76ecd575864beba2c3f265569dac214a9
Naman Khandelwal <namankoolwal1411@gmail.com>
2024-08-29 09:55:47 +0530

    fixed layout shifting in yourTalentOne section , remove locomotive package, add scrollbar css

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
-import LocomotiveScroll from 'locomotive-scroll';
-import Navbar from './components/Navbar'
-import LandingPage from './components/LandingPage/LandingPage'
-import Footer from './components/Footer'
 import { RouterProvider } from 'react-router-dom';
 import router from './router';
 import { ReactLenis } from 'lenis/react'
@@ -11,7 +7,6 @@ import { ReactLenis } from 'lenis/react'
 const App = () => {
  
 
-// const locomotiveScroll = new LocomotiveScroll();
   return (
     <ReactLenis root>
     <div className=' text-white w-full h-full '>
--- src/components/LandingPage/YourTalentOne.jsx
+++ src/components/LandingPage/YourTalentOne.jsx
@@ -1,26 +1,49 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import React from "react";
-import { useEffect, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
 import img1 from "../../assets/images/Landingpage/Yourtalent/img1.png";
 import img2 from "../../assets/images/Landingpage/Yourtalent/img2.png";
 import img3 from "../../assets/images/Landingpage/Yourtalent/img3.png";
-import frame from '../../assets/images/Landingpage/Yourtalent/frame.png'
+import frame from "../../assets/images/Landingpage/Yourtalent/frame.png";
 
 gsap.registerPlugin(ScrollTrigger);
 
+const YourtalentOne = ({ handelClick }) => {
+    const [imagesLoaded, setImagesLoaded] = useState(false);
 
-const YourtalentOne = ({handelClick}) => {
+    // Code to detect when all images are loaded
+    useEffect(() => {
+        const images = document.querySelectorAll('.gallery img');
+        let loadedImagesCount = 0;
 
-   
-    useEffect(() => { 
-        const elements = document.querySelectorAll('.details:not(:first-of-type) > *');
+        const handleImageLoad = () => {
+            loadedImagesCount += 1;
+            if (loadedImagesCount === images.length) {
+                setImagesLoaded(true); // Set state when all images are loaded
+            }
+        };
 
-        console.log(elements)
-        let ctx = gsap.context(() => {
+        images.forEach((img) => {
+            if (img.complete) {
+                handleImageLoad();
+            } else {
+                img.addEventListener('load', handleImageLoad);
+            }
+        });
+
+        return () => {
+            images.forEach((img) => img.removeEventListener('load', handleImageLoad));
+        };
+    }, []);
+
+
+    // ======================GSAP Animation======================
+    useEffect(() => {
+        if (imagesLoaded) {
             gsap.set(".photo:not(:first-child)", { opacity: 1, y: 1200 });
-            gsap.set(".details:not(:first-of-type) > *", { opacity: 0, y: 200 });
+            gsap.set(".details:not(:first-of-type) > *", { opacity: 0, y: 200  });
 
             const animation = gsap.to(".photo:not(:first-child)", {
                 opacity: 1,
@@ -33,8 +56,10 @@ const YourtalentOne = ({handelClick}) => {
                 opacity: 1,
                 y: 0,
                 duration: 1,
-                stagger: 2,
+                stagger: 3,
             });
+
+            
 
             ScrollTrigger.create({
                 trigger: ".gallery",
@@ -51,16 +76,13 @@ const YourtalentOne = ({handelClick}) => {
                 end: "bottom bottom",
                 animation: animation2,
                 scrub: true,
+                toggleActions: "play reverse play reverse",
                 // markers: true,
-            });      
-        });
+            });     
 
-        return () => ctx.revert();
-    }, []);
-
-
-    
-    
+            ScrollTrigger.refresh(); // Ensure ScrollTrigger is in sync
+        }
+    }, [imagesLoaded]);
 
     return (
         <div className="backgroundChange bg-black px-[5.5vw] py-20 my-[60px] md:my-[120px]">
@@ -77,92 +99,83 @@ const YourtalentOne = ({handelClick}) => {
                     </div>
                 </div>
             </div>
-            <div className=" gallery  mt-40 flex flex-col-reverse md:flex-row relative">
-                <div className="leftblock h-full w-full lg:w-1/2 ml-auto  -violet-200 ">
-                <div className="work-text w-[90%] mx-auto">
-                <div className="details w-3/4">
-                    <div className="first h-[100vw] lg:h-screen font-bold flex flex-col justify-center w-full ml-auto text-white gap-5">
-                        <h4 className="text-[1.75rem] text-[#EFAA76]">DISCOVER</h4>
-                        <div>
-                            <p className="text-[1.5rem] lg:text-[1.5rem] lg:w-3/4">
-                            Showcase your talent to a global and let opportunities find you.
-                            </p>
+            <div className="gallery mt-40 flex flex-col-reverse md:flex-row relative">
+                <div className="leftblock h-full w-full lg:w-1/2 ml-auto -violet-200 ">
+                    <div className="work-text w-[90%] mx-auto">
+                        <div className="details w-3/4">
+                            <div className="first h-[100vw] lg:h-screen font-bold flex flex-col justify-center w-full ml-auto text-white gap-5">
+                                <h4 className="text-[1.75rem] text-[#EFAA76]">DISCOVER</h4>
+                                <div>
+                                    <p className="text-[1.5rem] lg:text-[1.5rem] lg:w-3/4">
+                                        Showcase your talent to a global audience and let opportunities find you.
+                                    </p>
+                                </div>
+                                <div className="">
+                                    <button className="border border-[#EFAA76] font-normal px-5 py-2 rounded-md"
+                                        onClick={handelClick}
+                                    >
+                                        Contact Now
+                                    </button>
+                                </div>
+                            </div>
                         </div>
-                        <div className="">
-                            <button className="border border-[#EFAA76] font-normal px-5 py-2 rounded-md"
-                                onClick={handelClick}
-                            >
-                                Contact Now
-                            </button>
+                        <div className="details w-3/4">
+                            <div className="second h-[100vw] lg:h-screen font-bold flex flex-col justify-center w-full ml-auto text-white gap-5">
+                                <h4 className="text-[1.75rem] text-[#EFAA76]">CONNECT</h4>
+                                <div>
+                                    <p className="text-[1.5rem] lg:text-[1.5rem] lg:w-3/4">
+                                        Easily connect with artists tailored to your event needs and requirements.
+                                    </p>
+                                </div>
+                                <div className="">
+                                    <button className="border border-[#EFAA76] font-normal px-5 py-2 rounded-md">
+                                        Contact Now
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="details w-3/4">
+                            <div className="third h-[100vw] lg:h-screen font-bold flex flex-col justify-center w-full ml-auto text-white gap-5">
+                                <h4 className="text-[1.75rem] text-[#EFAA76]">HIRE</h4>
+                                <div>
+                                    <p className="text-[1.5rem] lg:text-[1.5rem] lg:w-3/4">
+                                        Hire the perfect artist by filtering options like genre, location, and availability.
+                                    </p>
+                                </div>
+                                <div className="">
+                                    <button className="border border-[#EFAA76] font-normal px-5 py-2 rounded-md">
+                                        Contact Now
+                                    </button>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-                    {/*************************  */}
-                <div className="details w-3/4">
-                    <div className="second h-[100vw] lg:h-screen font-bold flex flex-col justify-center w-full ml-auto text-white gap-5">
-                        <h4 className="text-[1.75rem] text-[#EFAA76]">CONNECT</h4>
-                        <div>
-                            <p className="text-[1.5rem] lg:text-[1.5rem] lg:w-3/4">
-                            Easily connect with artists tailored to your event needs and requirements.
-                            </p>
-                        </div>
-                        <div className="">
-                            <button className="border border-[#EFAA76] font-normal px-5 py-2 rounded-md">
-                                Contact Now
-                            </button>
-                        </div>
-                    </div>
-                </div>
-                    {/* *********************************** */}
-                <div className="details w-3/4">
+                <div className="rightblock backgroundChange bg-black md:py-10 lg:py-0 -green-400 sticky top-0 md:relative w-full lg:w-1/2 flex flex-col justify-start items-center lg:items-end md:pr-10">
+                    <div className="work-right-bl w-full md:h-screen md:sticky top-0 flex flex-col items-center md:items-end justify-center">
+                        <div className="my-24 relative md:my-0 w-[48vw] md:w-[20vw] h-[98vw] md:h-[41vw] flex justify-center ring-offset-2 z-[99] rounded-[35px] md:rounded-[50px] " style={{ backgroundImage: `url${frame}` }}>
 
-                    <div className="third h-[100vw] lg:h-screen font-bold flex flex-col justify-center w-full ml-auto text-white gap-5 ">
-                        <h4 className="text-[1.75rem] text-[#EFAA76]">HIRE</h4>
-                        <div>
-                            <p className="text-[1.5rem] lg:text-[1.5rem] lg:w-3/4">
-                            Hire the perfect artist by filtering options like genre, location, and availability.
-                            </p>
-                        </div>
-                        <div className="">
-                            <button className="border border-[#EFAA76] font-normal px-5 py-2 rounded-md">
-                                Contact Now
-                            </button>
+                            <img loading='lazy' src={frame} alt="" className="absolute z-10 w-full h-full" />
+
+                            <div className="relative w-[96%] h-full pt-0 overflow-hidden rounded-[32px] md:rounded-[32px] lg:rounded-[44px] ">
+                                <div className="photo absolute w-full h-full flex items-start justify-center">
+                                    <img loading='lazy'
+                                        src={img1}
+                                        alt="img1"
+                                        className="w-full h-full rounded-3xl object-fill "
+                                    />
+                                </div>
+
+                                <div className="photo absolute w-full h-full flex items-start justify-center">
+                                    <img loading='lazy' src={img3} alt="img3" className="w-full h-full rounded-3xl object-fill " />
+                                </div>
+
+                                <div className="photo absolute w-full h-full flex items-start justify-center">
+                                    <img loading='lazy' src={img2} alt="img2" className="w-full h-full rounded-3xl object-fill " />
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-                    {/* *********************************** */}
-                    </div>
-                </div>
-                <div className="rightblock backgroundChange bg-black  md:py-10 lg:py-0 -green-400 sticky top-0 md:relative w-full lg:w-1/2  flex flex-col justify-start items-center lg:items-end md:pr-10">
-                <div className="work-right-bl w-full md:h-screen md:sticky top-0 flex flex-col items-center md:items-end justify-center">
-                    <div className="my-24 relative md:my-0 w-[48vw]  md:w-[20vw] h-[98vw] md:h-[41vw] flex justify-center  ring-offset-2 z-[99]  rounded-[35px] md:rounded-[50px] " style={{backgroundImage : `url${frame}`}}>
-
-                    <img loading='lazy' src={frame} alt="" className="absolute z-10 w-full h-full" />
-
-
-                    <div className="relative w-[96%] h-full pt-0  overflow-hidden rounded-[32px] md:rounded-[32px] lg:rounded-[44px]  ">
-                        <div className="photo absolute w-full h-full flex items-start justify-center">
-                            <img loading='lazy'
-                                src={img1}
-                                alt="img1"
-                                className="w-full h-full rounded-3xl  object-fill "
-                            />
-                        </div>
-                       
-
-                        <div className="photo absolute w-full h-full flex items-start justify-center">
-                            <img loading='lazy' src={img3} alt="img2" className="w-full h-full rounded-3xl  object-fill " />
-                        </div>
-
-                        <div className="photo absolute w-full h-full flex items-start justify-center">
-                            <img loading='lazy' src={img2} alt="img2" className="w-full h-full rounded-3xl  object-fill " />
-                        </div>
-                        </div>
-
-
-
-                    </div>
-                </div>    
                 </div>
             </div>
         </div>
@@ -170,6 +183,3 @@ const YourtalentOne = ({handelClick}) => {
 };
 
 export default YourtalentOne;
-
-
-
